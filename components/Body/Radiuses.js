d929602de1e0ac8759a1b1a97a3ff14d1f990a3a
Phil Plückthun <phil@plckthn.me>
2017-11-07 02:08:44 +0000

    Optimise Radiuses animation

--- components/Body/Radiuses.js
+++ components/Body/Radiuses.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
 
-const range = (from = 0, to = 1) => fn => {
-  const arr = new Array(to - from + 1);
-  for (let i = from; i <= to; i++) {
-    arr.push(fn(i));
+const range = (from = 0, to = 1, step = 1) => fn => {
+  const arr = new Array(Math.floor((to - from + 1) / step));
+  for (let i = from; i <= to; i += step) {
+    arr[i] = fn(i);
   }
   return arr;
 };
@@ -25,49 +25,46 @@ const Wrapper = styled.svg.attrs({
 
 const breathe = (magnitude = 1.1) => keyframes`
   0% { transform: scale(1, 1) translate3d(0, 0, 0); }
-  33% { transform: scale(${magnitude}, ${magnitude}) translate3d(1%, -1%, 0); }
-  66% { transform: scale(1, 1) translate3d(-1%, 1%, 0); }
+  33% { transform: scale(${magnitude}, ${magnitude}) translate3d(2px, -2px, 0); }
+  66% { transform: scale(1, 1) translate3d(-2px, 2px, 0); }
   100% { transform: scale(${magnitude}, ${magnitude}) translate3d(0, 0, 0); }
 `;
 
-const breatheSmall = breathe(1.06);
-const breatheLarge = breathe(1.1);
+const breatheSmall = breathe(1.05);
+const breatheLarge = breathe(1.09);
 
-const Circle = styled.circle.attrs({
-  cx: 0,
-  cy: 0,
-  strokeWidth: 1,
-  stroke: 'rgba(253, 242, 255, 0.11)',
-  fill: 'none'
-})`
-  will-change: transform;
-  animation: ${p => (p.breatheSmall ? 8 : 10)}s ease-in-out
-    ${p => p.breathePhase * (p.breatheSmall ? 8 : 10)}s infinite alternate
-    ${p => (p.breatheSmall ? breatheSmall : breatheLarge)};
+const Breathe = styled.g`
+  transform: translate3d(0, 0, 0);
+  transform-origin: center center;
+  animation: ${p => (p.isSmall ? 10 : 12)}s ease-in-out ${p => p.phase * (p.isSmall ? 10 : 12)}s
+    infinite alternate ${p => (p.isSmall ? breatheSmall : breatheLarge)};
 `;
 
-Circle.defaultProps = {
-  breathePhase: 0,
-  breatheSmall: false
+Breathe.defaultProps = {
+  phase: 0,
+  isSmall: false
 };
+
+const Circle = ({ r }) => (
+  <circle r={r} cx={0} cy={0} strokeWidth={1} stroke="rgba(253, 242, 255, 0.11)" fill="none" />
+);
 
 class Radiuses extends Component {
   shouldComponentUpdate() {
     return false; // since this is static artwork, updates aren't necessary
   }
 
+  renderCircle(i) {
+    return <Circle key={i} r={20 * i} />;
+  }
+
   render() {
     return (
       <Wrapper>
         <g transform="translate(720, 430)">
-          {range(1, 760 / 20)(i => (
-            <Circle
-              key={i}
-              breathePhase={i % 2 === 0 ? 0 : 0.45}
-              breatheSmall={i % 2 === 0}
-              r={20 * i}
-            />
-          ))}
+          <Breathe isSmall>{range(2, 760 / 20, 2)(this.renderCircle)}</Breathe>
+
+          <Breathe phase={0.45}>{range(1, 760 / 20, 2)(this.renderCircle)}</Breathe>
         </g>
       </Wrapper>
     );
